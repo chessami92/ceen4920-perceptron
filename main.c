78f254ba42ebc3dce3853159b9700059a77f0ea9
Josh DeWitt <chessami92@gmail.com>
2013-06-16 22:43:05 -0500

    Now very accurately predicting phase and amplitude of a simple sinusoid.

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include "perceptron.h"
+#include "random.h"
 
 #define PERCEPTRON_INPUTS 5
 
-#include "perceptron.h"
-
 float nextValue( void ) {
-    static float time = -0.01;
+    static float time = 0;
     time += 0.01;
-    return sin( 2 * M_PI * 10 * time );
+    return 0.5 * sin( 2 * M_PI * 10 * time );
 }
 
 void advanceInputs( Vector *inputs, float nextValue ) {
@@ -22,22 +22,40 @@ void advanceInputs( Vector *inputs, float nextValue ) {
 }
 
 int main( int argc, char *argv[] ) {
+    initRand();
+
     float inputsA[PERCEPTRON_INPUTS];
     float weightsA[PERCEPTRON_INPUTS];
     Vector inputs;
-    inputs.a = inputsA; inputs.elements = PERCEPTRON_INPUTS;
     Vector weights;
-    weights.a = weightsA; weights.elements = PERCEPTRON_INPUTS;
     TestCase testCase = {&inputs, 0.0};
+    float output, error;
     int i;
+
+    inputs.a = inputsA; inputs.elements = PERCEPTRON_INPUTS;
+    weights.a = weightsA; weights.elements = PERCEPTRON_INPUTS;
+
+    for( i = 0; i < weights.elements; ++i ) {
+        weights.a[i] = randFloat() / 20;
+    }
 
     for( i = 0; i < inputs.elements; ++i ) {
         advanceInputs( &inputs, nextValue() );
     }
 
-    for( i = 0; i < 1; ++i ) {
+    for( i = 0; i < 10000; ++i ) {
         testCase.desiredOutput = nextValue();
         updateWeights( &testCase, &weights );
+        advanceInputs( &inputs, testCase.desiredOutput );
+    }
+
+    float nextVal;
+    for( i = 0; i < 250; ++ i ) {
+        nextVal = nextValue();
+        output = getOutput( weightedSum( &inputs, &weights ) );
+        error = fabs( output - nextVal );
+        printf( "%f,%f\n", nextVal, output );
+        advanceInputs( &inputs, output );
     }
 
     return 0;
